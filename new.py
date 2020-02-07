2c179cf73f6695ab241f999c1b9c7f5dd7805387
Elliot Hayward <vu19320@it075679.wks.bris.ac.uk>
2020-02-07 11:52:10 +0000

    Change Scripts again

--- new.py
+++ new.py
@@ -1,1 +1,5 @@
+#! /usr/bin/python3print("Welcome to my repository")
 #New python script
+#changing this scrypt
+if X == 10:
+	print('X is bigger than 10')
--- welcome.py
+++ welcome.py
@@ -1,2 +1,8 @@
 #! /usr/bin/python3print("Welcome to my repository")
-print("Welcome to my repository")
+print("Welcome to my repository") #change this line
+print("There isn't much here yet")
+def LargeX(X):
+	if X > 5:
+		print('X is large')
+	else:
+		print('X is small')
